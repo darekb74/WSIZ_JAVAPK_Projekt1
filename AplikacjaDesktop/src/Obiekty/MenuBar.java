f5e15b08d7ba0499352d84c412fad1dd7042ebcc
darekb74 <darekb.wsiz@gmail.com>
2018-04-23 18:43:42 +0200

    ActionListener
    ActionListener

--- AplikacjaDesktop/src/Obiekty/MenuBar.java
+++ AplikacjaDesktop/src/Obiekty/MenuBar.java
@@ -5,6 +5,8 @@
  */
 package Obiekty;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -14,32 +16,55 @@ import javax.swing.JMenuItem;
  *
  * @author Darek Xperia
  */
-public class MenuBar extends JMenuBar{
-    
+public class MenuBar extends JMenuBar implements ActionListener {
+
     public MenuBar(int poziomDostepu) {
         super();
-        
+
         JMenu menu, subMenu;
         JMenuItem menuItem;
-        
+
         // doajemy całe menu a potem wyłączamy elementy niedostępne dla danego poziomu
         menu = new JMenu("Program");
         menu.setMnemonic(KeyEvent.VK_P);
         add(menu);
         menuItem = new JMenuItem("Zakończ", KeyEvent.VK_Z);
+        menuItem.addActionListener(this);
         menu.add(menuItem);
-        
+
         menu = new JMenu("Magazyn");
         menu.setMnemonic(KeyEvent.VK_M);
         add(menu);
         menuItem = new JMenuItem("Sprawdź stany magazynowe", KeyEvent.VK_S);
-        if ((poziomDostepu & (Def.LVL1 | Def.LVL5)) == 0) menuItem.setEnabled(false); // brak uprawnień
+        menuItem.addActionListener(this);
+        if ((poziomDostepu & (Def.LVL1 | Def.LVL5)) == 0) {
+            menuItem.setEnabled(false); // brak uprawnień
+        }
         menu.add(menuItem);
         menuItem = new JMenuItem("Szukaj części", KeyEvent.VK_C);
-        if ((poziomDostepu & Def.LVL1) == 0) menuItem.setEnabled(false); // brak uprawnień
+        menuItem.addActionListener(this);
+        if ((poziomDostepu & Def.LVL1) == 0) {
+            menuItem.setEnabled(false); // brak uprawnień
+        }
         menu.add(menuItem);
-            
+
     }
-    
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem source = (JMenuItem) (e.getSource());
+        switch (source.getText()) {
+            case "Zakończ":
+                System.exit(0);
+                break;
+            case "Sprawdź stany magazynowe":
+                
+                break;
+            case "Szukaj części":
+                
+                break;
+            default:
+                break;
+        }
+    }
 }
