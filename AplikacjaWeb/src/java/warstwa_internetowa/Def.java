95dec9005267e402f84c04afbbfc6f29007342bb
darekb74 <darekb.wsiz@gmail.com>
2018-06-17 19:45:33 +0200

    login + logout

---
+++ AplikacjaWeb/src/java/warstwa_internetowa/Def.java
@@ -0,0 +1,24 @@
+package warstwa_internetowa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Darek Xperia
+ */
+public class Def {
+    public final static byte LVL0 = (byte) 0x0;    // 00000000 - podstawowe menu
+    public final static byte LVL1 = (byte) 0x1;    // 00000001 - menu pracownika magazynu
+    public final static byte LVL2 = (byte) 0x2;    // 00000010 - menu serwisanta 
+    public final static byte LVL3 = (byte) 0x4;    // 00000100 - 
+    public final static byte LVL4 = (byte) 0x8;    // 00001000 - 
+    public final static byte LVL5 = (byte) 0x10;   // 00010000 - dodawanie przesuniec magazynowych
+    public final static byte LVL6 = (byte) 0x20;   // 00100000 - dodawanie danych osobowych
+    public final static byte LVL7 = (byte) 0x40;   // 01000000 - 
+    public final static byte LVL9 = (byte) 0x80;   // 10000000 -                                              
+    public final static byte ADM = (byte) 0xFF;    // 11111111 - wszystkie (administrator)
+}
--- AplikacjaWeb/src/java/warstwa_internetowa/menadzer_uzytkownikow.java
+++ AplikacjaWeb/src/java/warstwa_internetowa/menadzer_uzytkownikow.java
@@ -5,24 +5,126 @@
  */
 package warstwa_internetowa;
 
-import javax.ejb.Stateless;
+import DTO.UserDTO;
+import EE_ejb.FasadaUserD_ejbRemote;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
  * @author Darek Xperia
  */
-@Stateless
+@ManagedBean
+@SessionScoped
 @Named(value = "menadzer_uzytkownikow")
-public class menadzer_uzytkownikow {
+public class menadzer_uzytkownikow implements Serializable {
+
+    private FasadaUserD_ejbRemote fasadaUser;
+
+    @PostConstruct
+    public void init() {
+        uDTO = null;
+        fasadaUser = lookupFasadaUserD_ejbRemote();
+        zalogowany = false;
+    }
+
+    private boolean zalogowany;
+    private UserDTO uDTO;
+
+    private String uzytkownik;
+    private String haslo;
+
+    public boolean isZalogowany() {
+        System.out.println("Zalogowany:" + zalogowany);
+        return zalogowany;
+    }
+
+    public String getUzytkownik() {
+        return uzytkownik;
+    }
+
+    public void setUzytkownik(String uzytkownik) {
+        this.uzytkownik = uzytkownik;
+    }
+
+    public String getHaslo() {
+        return haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
+    }
+
+    public FasadaUserD_ejbRemote getFasadaUser() {
+        return fasadaUser;
+    }
+
+    public void setFasadaUser(FasadaUserD_ejbRemote fasadaUser) {
+        this.fasadaUser = fasadaUser;
+    }
+
+    public String logout() {
+        if (zalogowany) {
+            uDTO = null;
+            zalogowany = false;
+            System.out.println("LOGOUT");
+        }
+        return "index";
+    }
+
+    public String login() {
+        System.out.println("Uzytkownik:" + uzytkownik + ", HASLO:" + haslo + ", FASADA:" + fasadaUser.toString());
+        if (fasadaUser != null && uzytkownik != null && haslo != null) {
+            uDTO = fasadaUser.znajdzUzytkownika(uzytkownik);
+            if (uDTO != null) {
+                if (uDTO.getPassword_hash().equals(Utils.Utils.md5(String.valueOf(haslo)))) {
+                    zalogowany = true;
+                    System.out.println("LOGIN");
+                } else {
+                    uDTO = null;
+                    zalogowany = false;
+                }
+            }
+        }
+
+        return "index";
+    }
 
     public boolean posiadaUprawnienia(String typ) {
-        switch (typ) {
-            default: 
-                return false;
-            case "magazynier":
-                return true;
+        if (!isZalogowany() || uDTO == null) {
+            return false;
         }
-        
+        switch (typ) {
+            case "magazynier":
+                if ((uDTO.getRmask() & Def.LVL1) > 0) {
+                    return true;
+                }
+                break;
+            case "administrator":
+                if ((uDTO.getRmask() & Def.ADM) > 0) {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    private FasadaUserD_ejbRemote lookupFasadaUserD_ejbRemote() {
+        try {
+            Context c = new InitialContext();
+            return (FasadaUserD_ejbRemote) c.lookup("ejb/FasadaUserD_ejb");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
     }
 }
